2656b970b45e15f6193a4a1e30959009323ff6ba
vquiros1996 <viviqm127@hotmail.com>
2023-10-02 16:42:35 -0600

    Creacion proyecto inicial

---
+++ src/main/java/com/Tienda_III/TiendaIiiApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_III;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tienda_III/TiendaIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda_III;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaIiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
